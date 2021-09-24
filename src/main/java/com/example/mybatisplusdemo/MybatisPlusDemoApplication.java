947363d0dd33b47b62c710e59e761be2a25c9264
YouJiaming <jiaming.you@thoughtworks.com>
2021-09-24 10:57:44 +0800

    [youjiaming]: a

--- src/main/java/com/example/mybatisplusdemo/MybatisPlusDemoApplication.java
+++ src/main/java/com/example/mybatisplusdemo/MybatisPlusDemoApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MybatisPlusDemoApplication {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusDemoApplication.class, args);
         System.out.println("hello");
